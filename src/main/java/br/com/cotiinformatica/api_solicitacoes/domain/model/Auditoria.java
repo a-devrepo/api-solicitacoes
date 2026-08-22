3f3eec1c8250dbf3d945784cf41a0a08a13c5b3c
alison <adevpr11@gmail.com>
2026-08-21 21:57:05 -0300

    feat: adiciona parâmetro de configuração na url de conexão com o mongodb e troca mapeamento da entidade

--- src/main/java/br/com/cotiinformatica/api_solicitacoes/domain/model/Auditoria.java
+++ src/main/java/br/com/cotiinformatica/api_solicitacoes/domain/model/Auditoria.java
@@ -1,7 +1,7 @@
 package br.com.cotiinformatica.api_solicitacoes.domain.model;
 
-import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
